009ec7f18adb540fb1fa0b19b31493476260ac05
Juha Rantanen <juha.rantanen@aalto.fi>
2014-12-20 18:44:36 +0200

    Interval 20ms. Cleanup.

--- effects/bass_rainbow.py
+++ effects/bass_rainbow.py
@@ -102,7 +102,7 @@ class Effect(object):
 effect = Effect()
 
 # You can play with the parameters here (quiet=False to print the magnitudes for example)
-spectrum = GstSpectrumDump(source='autoaudiosrc', vumeter=False, quiet=True, bands=45, interval=10,callback=effect.receive_magnitudes)
+spectrum = GstSpectrumDump(source='autoaudiosrc', vumeter=False, quiet=True, bands=45, interval=20,callback=effect.receive_magnitudes)
 spectrum.start()
 
 while not hyperion.abort():
@@ -113,46 +113,3 @@ while not hyperion.abort():
 
 
 spectrum.stop()
-
-
-
-
-# import hyperion
-# import time
-# import colorsys
-
-# # Get the parameters
-# rotationTime = float(hyperion.args.get('rotation-time', 3.0))
-# brightness = float(hyperion.args.get('brightness', 1.0))
-# saturation = float(hyperion.args.get('saturation', 1.0))
-# reverse = bool(hyperion.args.get('reverse', False))
-
-# # Check parameters
-# rotationTime = max(0.1, rotationTime)
-# brightness = max(0.0, min(brightness, 1.0))
-# saturation = max(0.0, min(saturation, 1.0))
-
-# # Initialize the led data
-# ledData = bytearray()
-# for i in range(hyperion.ledCount):
-#     hue = float(i)/hyperion.ledCount
-#     rgb = colorsys.hsv_to_rgb(hue, saturation, brightness)
-#     ledData += bytearray((int(255*rgb[0]), int(255*rgb[1]), int(255*rgb[2])))
-
-# # Calculate the sleep time and rotation increment
-# increment = 3
-# sleepTime = rotationTime / hyperion.ledCount
-# while sleepTime < 0.05:
-#     increment *= 2
-#     sleepTime *= 2
-# increment %= hyperion.ledCount
-
-# # Switch direction if needed
-# if reverse:
-#     increment = -increment
-
-# # Start the write data loop
-# while not hyperion.abort():
-#     hyperion.setColor(ledData)
-#     ledData = ledData[-increment:] + ledData[:-increment]
-#     time.sleep(sleepTime)

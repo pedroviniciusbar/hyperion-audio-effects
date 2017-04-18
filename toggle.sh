4452970fa500f55b308926a180cb7fb6dc8fa266
Juha Rantanen <juha@codercoded.com>
2017-04-18 11:17:29 +0300

    Added a simple toggle script.

---
+++ toggle.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# WARNING: This kills first python process it finds, pidof should be adjusted to find this exact script
+
+PROCESS='python'
+CMDPATH='.' # set path to hyperion-audio-effects
+CMD='python main.py'
+
+if pidof -s -x $PROCESS > /dev/null
+then
+    # Killing process
+    kill $(pgrep $PROCESS)
+    hyperion-remote -c black > /dev/null
+    sleep 3;
+    hyperion-remote -a localhost:20444 -c black > /dev/null
+    echo "$PROCESS was running. Killed it."
+else
+    # Starting process
+    cd $CMDPATH &&
+    $CMD  > /dev/null 2>&1 &
+    echo "$PROCESS was not running. Starting $CMD in $CMDPATH."
+fi
